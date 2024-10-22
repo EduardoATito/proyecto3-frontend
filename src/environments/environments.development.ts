512d06d6f01f2d36493c443bc9d2efdad28dae7e
Eduardo Apata <eduardotito.jose01@gmail.com>
2024-10-21 22:25:24 -0300

    delete environments

--- src/environments/environments.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://3.15.7.223:3000',
-};
--- src/environments/environments.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://3.15.7.223:3000',
-};
